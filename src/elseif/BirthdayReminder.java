e6697ca21f697befc7ac9bdeca19ffde65e6d304
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-01-02 19:19:33 -0800

    BirthdayReminder CrazyCatLady

--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,14 +11,37 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
-
+		String momsBirthday = "August 25th";
+		String dadsBirthday = "June 5th";
+		String myBirthday = "July 15th";
+		String brotherBirthday = "August 9th";
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String Frick = JOptionPane.showInputDialog("What birthday do you want?");
 		// 3. Print out what the user typed
+	 if (Frick.equals("Mom")) {
+		System.out.println(momsBirthday);
+		JOptionPane.showMessageDialog(null,momsBirthday);
+	}
+	 if (Frick.equals ("Dad")) {
+			System.out.println(dadsBirthday);
+			JOptionPane.showMessageDialog(null,dadsBirthday);
+
+		} 
+	 if (Frick.equals("Me")) {
+			System.out.println(myBirthday);
+			JOptionPane.showMessageDialog(null,myBirthday);
 		
+	 }
+	 if (Frick.equals("Brother")) {
+			System.out.println(brotherBirthday);
+			JOptionPane.showMessageDialog(null,brotherBirthday);
+		
+	 }
+	 else {
+		System.out.println("Sorry, i don't remember that person's birthday!");
+		JOptionPane.showMessageDialog(null,"Sorry, i don't remember that person's birthday!");
+	}
+	
 		// 4. if user asked for "mom"
 			//print mom's birthday
 		// 5. if user asked for "dad"
