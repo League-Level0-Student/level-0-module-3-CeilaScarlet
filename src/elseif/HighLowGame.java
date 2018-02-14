8496a798c1e3a249a5bcbfd786aa2c557d768ae9
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-13 19:22:18 -0800

    HighLowGame RobotColorChooser

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,26 +5,42 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
-		// 2. Print out the random variable above
 		
+		// 2. Print out the random variable above
+
 		// 11. do the following 10 times
 		
 			// 1. ask the user for a guess using a pop-up window, and save their response 
-
+String Gob = JOptionPane.showInputDialog("Guess a number?");
 			// 4. convert the users’ answer to an int (Integer.parseInt(string))
-			
+	int con=Integer.parseInt(Gob);	
+	
+	int random = new Random().nextInt(100);
+	
+	System.out.println(random);		
 			// 5. if the guess is correct
 				// 6. win
+	if (con==random) {
+		JOptionPane.showMessageDialog(null, "You won!!!!!!");
+	}
 			// 7. if the guess is high
 				// 8. tell them it's too high
+	if (con>random) {
+		JOptionPane.showMessageDialog(null, "It's too high!!!!!!");
+		JOptionPane.showMessageDialog(null, "You lost!!!!!!");
+	}
 			// 9. if the guess is low
 				// 10. tell them it's too low
-
+if (con<random) {
+	JOptionPane.showMessageDialog(null, "It's too low!!!!!!");
+	JOptionPane.showMessageDialog(null, "You lost!!!!!!");
+}
 		// 12. tell them they lose
 	}
 
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -5,14 +5,24 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
+
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
-
+Robot got= new Robot();
 		//3. ask the user what color they would like the robot to draw
-		
+String color=	JOptionPane.showInputDialog("What color do you like the pen to be?");
 		//4. use an if/else statement to set the pen color that the user requested
+if (color.equals("red")) {
+	got.setPenColor(Color.red);
+} else if(color.equals("blue")) {
+got.setPenColor(Color.blue);}
+
+
 
         //5. if the user doesn’t enter anything, choose a random color
 
@@ -21,7 +31,17 @@ public class RobotColorChooser {
 		//2. set the pen width to 10
 		
 	    //1. make the robot draw a shape (this will take more than one line of code)
-
-
+got.setX(200);
+got.setY(200);
+got.penDown();
+got.setPenWidth(10);
+got.setSpeed(10);
+got.move(100);
+got.turn(90);
+got.move(100);
+got.turn(90);
+got.move(100);
+got.turn(90);
+got.move(100);
 	}
 }
